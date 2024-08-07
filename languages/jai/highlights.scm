7e6935e6807b42002b752a9188e269380213b8ea
Stowy <stowy@protonmail.ch>
2024-08-07 18:16:42 +0200

    Upgraded tree sitter version and added new highlight queries

--- languages/jai/highlights.scm
+++ languages/jai/highlights.scm
@@ -1,24 +1,218 @@
-"if" @keyword
-"return" @keyword
-"else" @keyword
-"for" @keyword
-"while" @keyword
-"using" @keyword
-"struct" @type
+; Includes
 
-;(string_literal) @string
-(_type) @type
+[
+  "#import"
+  "#load"
+] @include
+
+; Keywords
+; TODO : complete this list
+[
+  "struct"
+  "enum"
+  "defer"
+  ;"cast"
+  ;"xx"
+] @keyword
+
+[
+  "return"
+] @keyword.return
+
+[
+  "if"
+  "else"
+  "case"
+  "break"
+] @conditional
+
+((if_expression
+  [
+    "then"
+    "ifx"
+    "else"
+  ] @conditional.ternary)
+  (#set! "priority" 105))
+
+; Repeats
+
+[
+  "for"
+  ;"do" I'm not sure jai has do ?
+  "while"
+  "continue"
+] @repeat
+
+; Variables
+
+(identifier) @variable
+
+; Namespaces
+
+(import (identifier) @namespace)
+
+; Parameters
+
+(parameter (identifier) @parameter ":" "="? (identifier)? @constant)
+
+(default_parameter (identifier) @parameter ":=")
+
+(call_expression argument: (identifier) @parameter "=")
+
+; Functions
+
+(procedure_declaration (identifier) @type)
+
+(procedure_declaration (identifier) @function (procedure (block)))
+
+(call_expression function: (identifier) @function.call)
+
+; Types
+
+(type (identifier) @type)
+
+((type (identifier) @type.builtin)
+  (#any-of? @type.builtin
+    "bool"
+    "int" "s8" "s16" "s32" "s64"
+    "u8" "u16" "u32" "u64"
+    "string"))
+
+
+(struct_declaration (identifier) @type "::")
+
+(enum_declaration (identifier) @type "::")
+
+;(union_declaration (identifier) @type "::")
+
+(const_declaration (identifier) @type "::" [(array_type) (pointer_type)])
+
+(struct . (identifier) @type)
+
+;(field_type . (identifier) @namespace "." (identifier) @type)
+
+;(bit_set_type (identifier) @type ";")
+
+;(procedure_type (parameters (parameter (identifier) @type)))
+
+;(polymorphic_parameters (identifier) @type)
+
+((identifier) @type
+  (#lua-match? @type "^[A-Z][a-zA-Z0-9]*$")
+  (#not-has-parent? @type parameter procedure_declaration call_expression))
+
+; Fields
+
+(member_expression "." (identifier) @field)
+
+;(struct_type "{" (identifier) @field)
+
+(struct_field (identifier) @field "="?)
+
+(field (identifier) @field)
+
+; Constants
+
+((identifier) @constant
+  (#lua-match? @constant "^_*[A-Z][A-Z0-9_]*$")
+  (#not-has-parent? @constant type parameter))
+
+(member_expression . "." (identifier) @constant)
+
+(enum_declaration "{" (identifier) @constant)
+
+; Literals
+
 (number) @number
-(function_definition name: (identifier) @function)
-;(struct_decl name: (identifier) @type.user)
-;(constant_value_definition name: (identifier) @constant)
-(comment) @comment
-;(block_comment) @comment
-;(cast_expression name: (identifier) @function)
-;(function_call name: (variable_reference) @function)
-(variable_definition name: (identifier) @variable)
-(variable_definition names: (identifier) @variable)
-;(implicit_variable_decl name: (identifier) @variable)
-(parameter_list name: (identifier) @variable)
-;(for_loop name: (identifier) @variable)
-;(for_loop names: (identifier) @variable)
+
+(float) @float
+
+(string) @string
+
+;(character) @character
+
+(escape_sequence) @string.escape
+
+(boolean) @boolean
+
+[
+  (uninitialized)
+  (null)
+] @constant.builtin
+
+((identifier) @variable.builtin
+  (#any-of? @variable.builtin "context"))
+
+; Operators
+
+[
+  ":="
+  "="
+  "+"
+  "-"
+  "*"
+  "/"
+  "%"
+  "%%"
+  ">"
+  ">="
+  "<"
+  "<="
+  "=="
+  "!="
+  "~="
+  "|"
+  "~"
+  "&"
+  "&~"
+  "<<"
+  ">>"
+  "||"
+  "&&"
+  "!"
+  ".."
+  "+="
+  "-="
+  "*="
+  "/="
+  "%="
+  "&="
+  "|="
+  "^="
+  "<<="
+  ">>="
+  "||="
+  "&&="
+  "&~="
+  ;"..="
+  ;"..<"
+  ;"?"
+] @operator
+
+; Punctuation
+
+[ "{" "}" ] @punctuation.bracket
+
+[ "(" ")" ] @punctuation.bracket
+
+[ "[" "]" ] @punctuation.bracket
+
+[
+  "::"
+  "->"
+  "."
+  ","
+  ":"
+  ";"
+] @punctuation.delimiter
+
+; Comments
+
+[
+  (comment)
+  (block_comment)
+] @comment @spell
+
+; Errors
+
+(ERROR) @error
